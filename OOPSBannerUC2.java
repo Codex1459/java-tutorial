bcbe5161c377ca7b94e84ed4213684d7cdc8eec3
Codex1459 <codexhub58@gmail.com>
2026-02-17 08:25:56 +0530

    update the code

--- OOPSBannerUC2.java
+++ OOPSBannerUC2.java
@@ -1,7 +1,5 @@
 public class OOPSBannerUC2 {
-
     public static void main(String[] args) {
-
         System.out.println(
                 "  *****   " + "  *****   " + " *******  " + "  ******  ");
 
