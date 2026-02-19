c8ca7f4bbb3cbee47421766e2a07ffd14f506cfa
Codex1459 <codexhub58@gmail.com>
2026-02-20 01:16:42 +0530

    Adding codes

--- OOPSBannerUC2.java
+++ OOPSBannerUC2.java
@@ -1,3 +1,11 @@
 public class OOPSBannerUC2 {
-    public static void main()
+    public static void main(String[] args) {
+        System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *     * ");
+        System.out.println("*       *" + "*       *" + " *     * " + "*        ");
+        System.out.println("*       *" + "*       *" + " ******  " + "  *****  ");
+        System.out.println("*       *" + "*       *" + " *       " + "       * ");
+        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+    }
 }
